bb8964a37361213dc02edc9d99c23c55b5e07284
Christian-Paul-Ruales <christianpaulruales@gmail.com>
2019-12-06 19:59:30 -0500

    mi primer commit

---
+++ program.py
@@ -0,0 +1,5 @@
+print("-----------------Learning How to use GitHub ---------------------")
+print("git init")
+
+print("git config --global user.email \"you@example.com\"")
+print("git config --global user.name \"christian\"")
