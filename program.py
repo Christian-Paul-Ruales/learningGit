40f3ed991c27f1afee0b0bdcdc8ebc454caac6fc
Christian-Paul-Ruales <christianpaulruales@gmail.com>
2019-12-06 20:31:50 -0500

    actualizando la clase de fazt

--- program.py
+++ program.py
@@ -3,3 +3,4 @@ print("git init")
 
 print("git config --global user.email \"you@example.com\"")
 print("git config --global user.name \"christian\"")
+print("git checkout -- program.py")#volver en el tiempo
